c801ef403c8b4ae1ca48f4b9c71f2151961cc8ad
KortinBruh <133661504+KortinBruh@users.noreply.github.com>
2023-05-17 18:40:13 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+//var app = require('express')();
+//var http = require('http').Server(app);
+//var io = require('socket.io')(http);
+var express = require("express"); // окрема змінна для зручності
+var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('C:/node/public'))
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('send message', function(msg) {
+	io.emit('receive message', msg);
+  });
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
